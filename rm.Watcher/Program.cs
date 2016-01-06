d2fa45a24f163344b600b804ee2e28158718aedf
hippy <hippy2981@gmail.com>
2016-01-05 22:52:07 -0800

    Add watcher logic and windows service

---
+++ rm.Watcher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+
+namespace rm.Watcher
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Start();
+            Console.ReadKey();
+        }
+
+        public static void Start()
+        {
+            foreach (var key in ConfigurationManager.AppSettings.AllKeys)
+            {
+                if (!key.StartsWith("watch"))
+                {
+                    continue;
+                }
+                var value = ConfigurationManager.AppSettings[key];
+                var tokens = value.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length != 2)
+                {
+                    continue;
+                }
+                var source = tokens[0].Trim();
+                var target = tokens[1].Trim();
+                new Watcher(source, target).Run();
+            }
+        }
+    }
+}
---
+++ rm.WatcherWindowsService/WatcherService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace rm.WatcherWindowsService
+{
+    public partial class WatcherService : ServiceBase
+    {
+        public WatcherService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                rm.Watcher.Program.Start();
+            }
+            catch (Exception)
+            { }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
